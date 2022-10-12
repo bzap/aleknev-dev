af8c4b4558691c39134e6f39c1eff7b3966ad922
linas <linas@linass-Mac-mini.local>
2022-10-12 02:55:47 -0400

    further responsive updates to contact and title

--- aleknev_dev/components/Contact.js
+++ aleknev_dev/components/Contact.js
@@ -61,22 +61,23 @@ const Contact = () => {
 		<Box
 			position={'relative'}
 			maxW={'100%'}
-			px={{ sm: '5%', md: '10%', lg: '10%' }}
+			px={{ base: '3%', sm: '5%', md: '10%', lg: '10%' }}
 			w={'100%'}
 			id='0'
 			pt='10em'
 			pb='16em'>
-
-
 			<Flex
+			
 				direction={'column'}
-				gap={'8em'}>
+				gap={{ base: '2em', sm: '4em', md: '7em', lg: '8em' }}>
 				<Title
 					title={'Contact.'}
 					desc={'Reach out! Whether you want to get in touch for work, collaborations, or even just to chat.'}
 					page={'03'} />
 				<Flex
-					p={10}
+					p={{base: '2em', sm: '2.5em', md: '4em', lg: '5em'}}
+					w={'100%'}
+					position={'relative'}
 					bg={'whiteAlpha.600'}
 					boxShadow={'md'}
 					borderRadius={'22px'}
@@ -85,14 +86,14 @@ const Contact = () => {
 							style={{ flex: 1, width: "100%" }}
 							onSubmit={handleSubmit}>
 							<Flex
-								gap={'1em'}
+								gap={{ base: '2em', sm: '2.5em', md: '4em', lg: '4em' }}
 								direction={'column'}>
 								<FormControl
 									isRequired>
 									<Flex
 										h={'100%'}
 										w={'100%'}
-										direction={'row'}
+										direction={{base:'column', sm: 'column', md: 'row', lg: 'row'}}
 										gap={'2em'}>
 										<Flex
 											h={'100%'}
@@ -100,7 +101,7 @@ const Contact = () => {
 											direction={'column'}>
 											<FormLabel
 												requiredIndicator
-												fontSize={30}
+												fontSize={{ base: 22, sm: 23, md: 28, lg: 30 }}
 												fontWeight={900}>
 												EMAIL
 											</FormLabel>
@@ -118,7 +119,7 @@ const Contact = () => {
 											<FormLabel
 												requiredIndicator
 												pt={4}
-												fontSize={30}
+												fontSize={{ base: 22, sm: 23, md: 28, lg: 30 }}
 												fontWeight={900}>
 												NAME
 											</FormLabel>
@@ -138,7 +139,7 @@ const Contact = () => {
 											w={'100%'}>
 											<FormLabel
 												requiredIndicator
-												fontSize={30}
+												fontSize={{ base: 22, sm: 23, md: 28, lg: 30 }}
 												fontWeight={900}>
 												YOUR MESSAGE
 											</FormLabel>
@@ -159,7 +160,6 @@ const Contact = () => {
 									w={'100%'}
 									mt={4}
 									colorScheme={'blackAlpha'}
-									variant={'ghost'}
 									borderRadius={'12px'}
 									borderWidth={'1px'}
 									borderColor={'gray.500'}
