f6f41c7126a88b92c70658c21dca0d57ea018b0e
linas <linasalekne@gmail.com>
2022-11-01 02:47:33 -0400

    contacts update

--- aleknev_dev/projects/LazyImageAnimator.js
+++ aleknev_dev/projects/LazyImageAnimator.js
@@ -47,7 +47,7 @@ const LazyImageAnimator = ({ props }) => {
         setImg(false)
     }
     const imageData = props.src.split('_')
-    console.log(imageData[0], imageData[1], imageData[2])
+    console.log(imageData[0], imageData[1], imageData[2], '3')
     const src = { src: imageData[0], alt: props.alt }
     return (
         <Flex
