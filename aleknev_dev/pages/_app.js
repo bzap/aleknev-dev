e03e335c9339b96ff95cc9c8e0a91c881dbfe312
linas <linasalekne@gmail.com>
2022-11-18 20:32:59 -0500

    added new portrait and favicon, added page title and vercel page stats

--- aleknev_dev/pages/_app.js
+++ aleknev_dev/pages/_app.js
@@ -2,23 +2,29 @@ import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import { extendTheme } from '@chakra-ui/react';
 import '../styles/styles.css';
+import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
 
 const theme = extendTheme({
 	fonts: {
 		heading: `'sfheavy'`,
 		body: `'sfbold'`,
-  	},
+	},
 })
 
 function MyApp({ Component, pageProps, router }) {
 	return (
 		<>
-			<ChakraProvider 
+			<ChakraProvider
 				theme={theme}>
-					<Component {...pageProps} key={router.route} /> 
-	  		</ChakraProvider>
+				<Head>
+					<title>Aleknev</title>
+				</Head>
+				<Component {...pageProps} key={router.route} />
+				<Analytics />
+			</ChakraProvider>
 		</>
-  	) 
+	)
 }
 
 export default MyApp
